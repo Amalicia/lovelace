d54a1994204cca53559a19a08e6a2ce0f8914c66
Lottie Bryant <amaliciabryant@gmail.com>
2020-04-23 14:57:47 +0100

    Final hand-in

--- trainer/model.py
+++ trainer/model.py
@@ -9,16 +9,19 @@ import logging
 log = logging.getLogger(__name__)
 
 
+# function to convert numoy arrays to a tensorflow dataset
 def make_inputs(data, labels, batch_size, generator):
+	# use the from generator function with the keras generator
 	dataset = tf.data.Dataset.from_generator(
 		lambda: generator.flow(data, labels, batch_size=batch_size),
 		output_types=(tf.float32, tf.uint8),
-		output_shapes=([None, 224, 224, 3], [None, 6])
+		output_shapes=([None, 224, 224, 1], [None, 6])
 	)
 	return dataset
 
 
-# https://www.kaggle.com/arsenyinfo/f-beta-score-for-keras
+# Kravchenko, A. (2017) F-beta core for Keras. Kaggle. Available from: https://www.kaggle.com/arsenyinfo/f-beta-score-for-keras
+# [accessed 13 February 2020]
 def fbeta(y_pred, y_true, beta=2):
 	y_pred = tf.keras.backend.clip(y_pred, 0, 1)
 
@@ -35,10 +38,13 @@ def fbeta(y_pred, y_true, beta=2):
 	return fbeta_score
 
 
+# function to create the baseline model
 def create_baseline_model(input_dimensions, learning_rate, out_shape=6):
+	# use mirrored strategy for GPU training
 	strategy = tf.distribute.MirroredStrategy()
 	log.info('Number of devices: {}'.format(strategy.num_replicas_in_sync))
 	with strategy.scope():
+		# model definition
 		model = tf.keras.models.Sequential()
 		model.add(tf.keras.layers.Conv2D(32, (3, 3), activation=tf.nn.relu, kernel_initializer='he_uniform', padding='same',
 		                                 input_shape=input_dimensions))
@@ -58,29 +64,40 @@ def create_baseline_model(input_dimensions, learning_rate, out_shape=6):
 		model.add(tf.keras.layers.Flatten())
 		model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu, kernel_initializer='he_uniform'))
 		model.add(tf.keras.layers.Dense(out_shape, activation='sigmoid'))
-		# compile model
+
+		# create stochastic gradient descent optimiser
 		opt = tf.keras.optimizers.SGD(lr=learning_rate, momentum=0.9)
+		# create model
 		model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy', fbeta])
 	return model
 
 
+# create resnet model
 def create_resnet_model(input_dimensions, learning_rate, out_shape=6):
+	# mirrored strategy for GPU training
 	strategy = tf.distribute.MirroredStrategy()
 	log.info('Number of devices: {}'.format(strategy.num_replicas_in_sync))
 	with strategy.scope():
-		base_model = tf.keras.applications.resnet50.ResNet50(weights='imagenet', include_top=False, input_shape=input_dimensions)
+		# create base model using ResNet provided by kears
+		base_model = tf.keras.applications.resnet50.ResNet50(weights=None, include_top=False, input_shape=input_dimensions)
+
+		# define additional layers to the model
 		x = base_model.output
 		x = tf.keras.layers.GlobalAveragePooling2D()(x)
 		x = tf.keras.layers.Dropout(0.2)(x)
-
 		predictions = tf.keras.layers.Dense(out_shape, activation='sigmoid')(x)
+
+		# create complete model
 		model = tf.keras.models.Model(inputs=base_model.input, outputs=predictions)
 
+		# stochastic gradient descent optimiser
 		opt = tf.keras.optimizers.SGD(lr=learning_rate, momentum=0.9)
+		# create model
 		model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy', fbeta])
 	return model
 
 
+# create densenet model - identical to resnet but creates a densenet architecture
 def create_densenet_model(input_dimensions, learning_rate, out_shape=6):
 	strategy = tf.distribute.MirroredStrategy()
 	log.info('Number of devices: {}'.format(strategy.num_replicas_in_sync))
--- trainer/task.py
+++ trainer/task.py
@@ -18,6 +18,7 @@ from . import util
 from . import model
 from . import upload_file
 
+# tensorflow setup
 sess = tf.Session()
 graph = tf.get_default_graph()
 tf.keras.backend.set_session(sess)
@@ -30,7 +31,7 @@ console.setLevel(logging.INFO)
 log.addHandler(console)
 
 
-
+# argument parser
 def args():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--job-dir', type=str, required=True)
@@ -45,29 +46,28 @@ def args():
 	return arguments
 
 
-def remove_encoding(prediction, inv_map):
-	rounded = prediction.round()
-	tags = [inv_map[i] for i in range(len(rounded)) if rounded[i] == 1.0]
-	return tags
-
-
+# create model details file
 def details(args, keras_model, x_test, y_test, loss, accuracy, fbeta, batch_size):
 	log.info('Writing model details...')
 	f = open("model_info.txt", "a")
+	# write model name, epochs and batch size
 	f.write('**** {0} *****'.format(args.model_name))
 	f.write('Epochs: {0}\nBatch Size: {1}\n\n'.format(args.epochs, args.batch_size))
+
+	# write model summary to file
 	with open("model_info.txt", "w") as f:
 		with redirect_stdout(f):
 			keras_model.summary()
 	log.info('Written summary')
 
-	inv_map = {v: k for k, v in util.TAG_MAPPING.items()}
 	labels = list(util.TAG_MAPPING.keys())
 
+	# make predictions and round them to be between 0 and 1
 	log.info("Making predictions")
 	y_pred = keras_model.predict(x_test, batch_size=batch_size, verbose=1, steps=int(x_test.shape[0] / batch_size))
 	y_pred = y_pred.round().astype(np.int)
 
+	# write model statistics
 	log.info('Writing stats')
 	f = open("model_info.txt", "a")
 	f.write('Loss: %.3f\n' % loss)
@@ -75,8 +75,10 @@ def details(args, keras_model, x_test, y_test, loss, accuracy, fbeta, batch_size
 	f.write('F2 Score: %.3f\n\n' % fbeta)
 
 	log.info('Writing report + Confusion matrix')
+	# write classification report
 	f.write(classification_report(y_test, y_pred, target_names=labels))
 	log.info("Written report")
+	# write confusion matrices
 	cm = multilabel_confusion_matrix(y_test, y_pred)
 	for i in range(len(labels)):
 		f.write('Confusion Matrix for {0}:\n'.format(labels[i]))
@@ -85,11 +87,14 @@ def details(args, keras_model, x_test, y_test, loss, accuracy, fbeta, batch_size
 
 	f.close()
 	log.info('Done writing details!')
+	# upload file
 	cloud_file = 'models/{0}/{1}_info.txt'.format(args.data.lower(), args.model_name)
 	upload_file.upload_file('lovelace-data', "model_info.txt", cloud_file)
 
 
+# save and upload model to cloud storage
 def save_and_upload(args, keras_model):
+	# save and upload model
 	log.info('Saving model as h5...')
 	keras_model.save('model.h5')
 	cloud_model = 'models/{0}/{1}.h5'.format(args.data.lower(), args.model_name)
@@ -99,7 +104,9 @@ def save_and_upload(args, keras_model):
 	tf.saved_model.save(keras_model, save_loc)
 
 
+# train and evaluate model
 def train_and_evaluate(args):
+	# get data
 	x_train, y_train, x_test, y_test, x_val, y_val = util.load_data(args.data.lower())
 	log.info('Loaded data')
 
@@ -109,28 +116,35 @@ def train_and_evaluate(args):
 	test_samples = x_test.shape[0]
 	val_samples = x_val.shape[0]
 
-	keras_model = model.create_densenet_model(input_dimensions=input_dimensions, learning_rate=args.lr)
+	# create model
+	keras_model = model.create_resnet_model(input_dimensions=input_dimensions, learning_rate=args.lr)
 
+	# create keras generator for images
 	image_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1.0 / 255.0)
 
+	# get train and validation data as a tensorflow dataset
 	train_data = model.make_inputs(x_train, y_train, batch_size=args.batch_size, generator=image_generator)
 	validation_data = model.make_inputs(x_val, y_val, batch_size=args.batch_size, generator=image_generator)
 
 	del x_train, y_train, x_val, y_val
 	gc.collect()
 
+	# create learning rate callback
 	learning_rate_decay = tf.keras.callbacks.LearningRateScheduler(
 		lambda epoch: args.lr + 0.02 * (0.5 ** (1 + epoch))
 	)
 
+	# create tensorboard callback
 	tensorboard_cb = tf.keras.callbacks.TensorBoard(
 		os.path.join(args.job_dir, 'keras_tensorboard'),
 		histogram_freq=1)
 
+	# start trainging
 	log.info('Beginning training')
 	train_start = time.time()
 	with graph.as_default():
 		tf.keras.backend.set_session(sess)
+		# train model
 		keras_model.fit(train_data,
 		                steps_per_epoch=int(train_samples / args.batch_size),
 		                epochs=args.epochs,
@@ -143,27 +157,40 @@ def train_and_evaluate(args):
 	del train_data, validation_data
 	gc.collect()
 
+	# find optimal batch size for the test dataset so the data can be bacthed and the GPU's will not run out of memory
 	for i in range(1, 160):
 		if len(x_test) % i == 0:
 			div = i
 	test_batch = div
 
+	# evaluate model
 	log.info("Evaluating model")
+	# create tensorflow dataset representation of evaluation data
 	test_inputs = model.make_inputs(x_test, y_test, batch_size=test_batch, generator=image_generator)
 	log.info('Done making test inputs')
+	# evaluate model
 	loss, accuracy, fbeta = keras_model.evaluate(test_inputs, steps=int(test_samples / test_batch), verbose=1)
 	del test_inputs
 	gc.collect()
+	# log out the results
 	log.info('>>> loss=%.3f, accuracy=%.3f, f2=%.3f' % (loss, accuracy, fbeta))
+	# create model details file
 	details(args, keras_model, x_test, y_test, loss, accuracy, fbeta, test_batch)
+	# save and upload model
 	save_and_upload(args, keras_model)
 
 
+# main function
 if __name__ == '__main__':
 	# os.environ['GOOGLE_APPLICATION_CREDENTIALS']='bry16607715-f906f68756ff.json'
 	log.info('Starting...')
+	# start time - to measure how long the process takes
 	start_time = time.time()
+	# get argument data
 	args = args()
+	# set logging verbosity
 	tf.compat.v1.logging.set_verbosity('INFO')
+	# train and evaluate model
 	train_and_evaluate(args)
+	# report time taken to run the entrie script
 	log.info("Execution took: {0} seconds".format(time.time() - start_time))
